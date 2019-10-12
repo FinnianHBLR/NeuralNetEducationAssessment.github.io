598dbc2cd212ebf46e8d63b05ced3c2a2e65ba37
Finn <finnniansproject@gmail.com>
2019-10-12 13:31:34 +1300

    New stuff

---
+++ main.js
@@ -0,0 +1,5 @@
+function clearEmail() {
+    document.getElementById("inputSubject").value = "";
+    document.getElementById("inputBody").value = "";
+    //EMAILTO would be put here to get information from the id's above.
+}
